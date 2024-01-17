4ea5d8fd05a2651d5743667f6c0973712a0cabfc
Anja Klaßen <anja.klassen@posteo.de>
2024-01-17 15:54:37 +0100

    add solution

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import BoxWithStyledComponents from "../components/BoxWithStyledComponents.js";
+import styled, { css } from "styled-components";
+
+const FlexContainer = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 4px;
+  flex-direction: ${({ $direction = "row" }) =>
+    $direction === "row" ? "row" : "column"};
+`;
 
 export default function HomePage() {
   return (
     <div>
-      <BoxWithClassName />
-      <BoxWithClassName isBlack />
+      <FlexContainer $direction="row">
+        <BoxWithClassName />
+        <BoxWithClassName isBlack />
+        <BoxWithStyledComponents />
+        <BoxWithStyledComponents $isBlack />
+      </FlexContainer>
     </div>
   );
 }
